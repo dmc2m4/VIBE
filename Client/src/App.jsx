5797003ee1c3dc724008ed17f4c1a3f47f86adad
alfonzojosue <16-00436@usb.ve>
2023-01-19 08:47:35 -0400

    Add footer component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/Login/Login";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Login/>
+      <Footer/>
     </div>
   );
 }
--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import twitter from '../../assets/twitter.png'
+import facebook from '../../assets/facebook.png'
+import instagram from '../../assets/instagram.png'
+import style from './Footer.module.css'
+
+
+
 
 const Footer = () => {
   return (
-    <footer></footer>
+    <footer className={style.footerContainer}>
+        <div className={style.containerSection}>
+            <section>
+            <img src="" alt="Logo" />
+            </section>
+            <section>
+                <p>Terminos y condiciones</p>
+                <p>Defensa al consumidor</p>
+                <p>About us</p>
+                <p>Ayuda</p>
+            </section>
+            <section>
+            <h3>Nuestras redes:</h3>
+            <img src={twitter} alt="" className={style.socialImg} />
+            <img src={facebook} alt="" className={style.socialImg} />
+            <img src={instagram} alt="" className={style.socialImg} />
+            </section>
+        </div>
+    </footer>
   )
 }
 
