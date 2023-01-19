5d1331c63b60247c424e41a4df2f85249869ab99
Tomás Alcaraz <tomasalejandroalcaraz@gmail.com>
2023-01-19 20:18:05 -0300

    inicializando el estado, y modularizando las actions

--- Client/src/redux/actions/getAllChlotes.action.js
+++ Client/src/redux/actions/getAllChlotes.action.js
@@ -1,13 +1,12 @@
+import types from "./types";
 import axios from "axios";
-
-const GET_ALL_CHLOTHES = "GET_ALL_CHLOTHES";
 
 function getAllChlotes() {
   return async function (dispatch) {
     return await axios
       .get("/products")
       .then((res) => {
-        dispatch({ type: GET_ALL_CHLOTHES, payload: res.data });
+        dispatch({ type: types.GET_ALL_CHLOTHES, payload: res.data });
       })
       .catch((e) => e.message);
   };
