22ca8fa060c5e9861b154a2811ac1c9e1acf3c4a
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-01-18 20:45:34 -0500

    correcciones middelwares user

--- Api/src/Middlewares/PostUserMiddleware.js
+++ Api/src/Middlewares/PostUserMiddleware.js
@@ -1,12 +1,15 @@
+const {User} = require ('../db')
+
 const PUM = async function (req, res, next) {
+  const {email} = req.body
   const searchingUser = await User.findAll({
     where: {
-      email: value.email.toUpperCase(),
+      email: email.toUpperCase(),
     },
   });
   if (searchingUser.length) throw new Error("this user already exist");
   const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i;
-  if (!regexEmail.test(value.email)) throw new Error("this is not an email");
+  if (!regexEmail.test(email)) throw new Error("this is not an email");
 
   next();
 };
