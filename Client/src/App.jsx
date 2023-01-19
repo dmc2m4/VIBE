52ba4b04fc766006b0a7277e191ef525af1f5244
alfonzojosue <16-00436@usb.ve>
2023-01-19 18:01:43 -0400

    Add React Router Dom

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/Login/Login";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer/Footer.jsx";
+import { Route, Router, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-     <Footer/>
+      
+      <Routes>
+      <Route exact path='/footer'
+       element={<Footer/>}/>
+     </Routes>
+     
     </div>
   );
 }
