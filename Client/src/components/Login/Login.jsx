3ee216fca3d3d8666bbb0596df08df903452a41d
alfonzojosue <16-00436@usb.ve>
2023-01-20 12:14:06 -0400

    merge

--- Client/src/components/Login/Login.jsx
+++ Client/src/components/Login/Login.jsx
@@ -1,24 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import { NavLink } from "react-router-dom";
-
-const Login = () => {
-  return (
-    <div>
-      <form>
-        <label>Username:</label>
-        <input type="text" id="username" name="username" />
-        <label>Password:</label>
-        <input type="password" id="password" name="password" />
-        <button type="submit">Login</button>
-      </form>
-      <NavLink to="/signup">
-        <span>Crear Cuenta</span>
-      </NavLink>
-      <p>
-        Vincula cuanta con <span>Google</span>
-      </p>
-=======
 import style from './Login.module.css'
 
 const Login = () => {
@@ -32,7 +12,7 @@ const Login = () => {
         <input type='password' id='password' name='password' className={style.inputLogin} placeholder="Enter your Password" />
         <button type='submit' className={style.buttonLogin}>Login</button>
       </form>
->>>>>>> b4f3b69329c07f8d5037cae7c6f9ef13779a786c
+
     </div>
   );
 };
