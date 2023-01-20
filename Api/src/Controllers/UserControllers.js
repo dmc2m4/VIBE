494e5c1a72cea599a80b88a13878183eadeb6d04
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-01-19 21:13:54 -0500

    password encriptada

--- Api/src/Controllers/UserControllers.js
+++ Api/src/Controllers/UserControllers.js
@@ -1,25 +1,31 @@
 const { User } = require("../db.js");
+const bcrypt = require('bcrypt');
 
 const getAllUsers = async () => {
   const allUsers = await User.findAll();
   return allUsers;
 };
 
-const postUsers = async (value) => {
-  const newUser = await User.create(value);
-  return newUser;
-};
+const postUsers = async ({ name, password, password2, img, email }) => {
+    const cryptPassword = bcrypt.hashSync(password, 10);
+    const cryptPassword2 = bcrypt.hashSync(password2, 10);
+    const newUser = await User.create({
+      name: name,
+      password: cryptPassword,
+      password2:cryptPassword2,
+      email,
+      img,
+    })
+    return newUser;
+  }
 
-const deleteUsers = async (value) => {
-  await User.destroy({
-    where: {
-      id: value,
-    },
-  });
-};
+
+  const deleteUsers = async function (email){
+     await User.destroy({where: {email}})
+}
 
 const putUsers = async (value, req) => {
-const { name, password, email, img } = req
+  const { name, password, email, img } = req
   const update = await User.findByPk(value);
   if (name) update.name = name;
   if (password) update.password = password;
