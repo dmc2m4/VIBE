02d27fb4a736ab0c14d53d7d626b1f96103a7744
Tomás Alcaraz <tomasalejandroalcaraz@gmail.com>
2023-01-19 23:02:31 -0300

    Solucionando errores de enrutamiento

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,25 +5,19 @@ import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/Login/Login";
 import Landing from "./components/Landing/Landing";
 import FormSignUp from "./components/FormSignUp/FormSignUp";
-import Login from "./components/Login/Login";
+// import Login from "./components/Login/Login";
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={Landing} />
+        <Route exact path="/" element={<Landing />} />
         {/* <Route exact path="/home" component={Home}/> */}
-        <Route path="/signup" element={FormSignUp} />
-        <Route exact path="/login" element={Login} />
+        <Route path="/signup" element={<FormSignUp />} />
+        {/* <Route exact path="/login" element={Login} /> */}
       </Routes>
     </div>
   );
 };
-
-// const test1;
-console.log(test2);
-
-var test2; 
-
 
 export default App;
--- Client/src/components/Landing/Landing.jsx
+++ Client/src/components/Landing/Landing.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Login from "../Login/Login";
+import { NavLink } from "react-router-dom";
 
 const landing = () => {
   return (
     <div>
       {/* <img src='' alt=''></img> */}
-      <NavLink>
+      <NavLink to="login">
         <Login />
       </NavLink>
     </div>
