d71922233cc659d9b3bfbc4fde03fc23ef2550f3
Brian Policani <brianpolicani@hotmail.com>
2023-01-21 18:38:05 -0300

    arreglo de index general

--- Api/index.js
+++ Api/index.js
@@ -1,14 +1,29 @@
-const { Router } = require('express');
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const {PORT} =require("./config.js")
 
-// Importar todos los routers;
-const userRouter = require('./userRouter.js');
-const productRouter = require('./productRouter')
-const pagingRouter = require('./pagingRouter')
-const router = Router();
-
-// Configurar los routers
-router.use('/user', userRouter);
-router.use('/product', productRouter)
-router.use('/pagination', pagingRouter)
-
-module.exports = router;
+// Syncing all the models at once.
+conn.sync({ }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+  });
+});
