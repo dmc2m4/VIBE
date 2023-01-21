c5b4ca587385cb947dcea47cdebf51da5f5741dc
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-01-21 15:14:21 -0500

    cambio de ruta en action createUser

--- Client/src/redux/actions/createUser.js
+++ Client/src/redux/actions/createUser.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const createUser = (user) => {
   return async function () {
-    const response = await axios.post("http://localhost:3001/user", user);
+    const response = await axios.post("http://localhost:3001/user/signup", user);
     return response;
   };
 };
