25d8432e8803801e3e4257eca01c5dda732e583f
Brian Policani <brianpolicani@hotmail.com>
2023-01-22 20:53:52 -0300

    commit

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require('express');
 // Importar todos los routers;
 const userRouter = require('./userRouter.js');
 const productRouter = require('./productRouter')
+const pagingRouter = require('./pagingRouter')
 const router = Router();
 
 // Configurar los routers
 router.use('/user', userRouter);
 router.use('/product', productRouter)
+router.use('/pagination', pagingRouter)
 
 module.exports = router;
