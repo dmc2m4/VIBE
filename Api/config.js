bc0a0863552276d5ce0221c92a80e026f9a7be10
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-01-23 13:56:22 -0500

    agregando rutas a la navbar

--- Api/config.js
+++ Api/config.js
@@ -1,5 +1,5 @@
 const DB_USER = process.env.DB_USER || "postgres";
-const DB_PASSWORD = process.env.DB_PASSWORD || "alcaraz";
+const DB_PASSWORD = process.env.DB_PASSWORD || "Chelsea.52";
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_PORT = process.env.DB_PORT || "5432";
 const DB_NAME = process.env.DB_NAME || "vibes";
