48094271d23fe3e768864f8a8e56946a6c5d6847
Ramiro Soares gache <86982224+ramisoaresgache@users.noreply.github.com>
2023-01-22 21:41:20 -0300

    category-fix

--- Client/src/redux/actions/updateFilters.js
+++ Client/src/redux/actions/updateFilters.js
@@ -1,7 +1,6 @@
 import types from "./types";
 
 const updateFilters = (filtereds) => {
-  console.log(filtereds);
   return function (dispatch) {
     return dispatch({ type: types.UPDATE_FILTERS, payload: filtereds });
   };
