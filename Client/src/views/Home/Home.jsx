42f358dac05d713c07a200ee334c772f8c7ceb65
Ramiro Soares gache <86982224+ramisoaresgache@users.noreply.github.com>
2023-01-25 15:56:01 -0300

    arreglo search y modificaciones de los formularios eliminados

--- Client/src/views/Home/Home.jsx
+++ Client/src/views/Home/Home.jsx
@@ -1,25 +1,13 @@
 import React from "react";
-import Navbar from "../../components/Navbar/Navbar";
-import Products from "../../components/Products/Products";
 import Pagination from "../../components/Pagination/Pagination";
-import Filters from "../../components/Filters/Filters";
-import ContainerProduct from '../../containers/containerProductHome/ContainerProduct'
-import style from './Home.module.css'
-import Loading from "../../components/Loading/Loading";
-import { useSelector } from "react-redux";
-
+import ContainerProduct from "../../containers/containerProductHome/ContainerProduct";
+import style from "./Home.module.css";
 
 const Home = () => {
-  const product = useSelector ((state)=> state.Products)
   return (
     <div className={style.container}>
-    { !product.length ?
-      <Loading/>:
-      <div>
-      <ContainerProduct/> 
+      <ContainerProduct />
       <Pagination />
-      </div>
-    }
     </div>
   );
 };
