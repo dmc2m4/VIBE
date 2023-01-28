2344a1bec3390af4d49b7259872171f19bb409dc
Brian Policani <brianpolicani@hotmail.com>
2023-01-27 22:00:36 -0300

    borrar

--- Api/src/Middlewares/PostProductMiddleware.js
+++ Api/src/Middlewares/PostProductMiddleware.js
@@ -14,30 +14,6 @@ const MPP = function (req, res, next) {
   if (!season) throw new Error("Missing season");
   if (!stock) throw new Error("Missing stock");
   
- /*  const storage = multer.diskStorage({
-    filename: (req, file, cb) => {
-      //seteo el nombre de las fotos, en este caso un uuid
-      cb(null, uuid() + path.extname(file.originalname).toLocaleLowerCase());
-    },
-  });
-  
-  const mMulter = multer({
-    storage: storage,
-    limits: { fileSize: 4000000 }, //tamaño maximo de la imagen
-    fileFilter: (req, file, cb) => {
-      //aca checkeo de que sea un formato imagen
-      const filetypes = /jpeg|jpg|png/;
-      const mimetype = filetypes.test(file.mimetype);
-      const extname = filetypes.test(path.extname(file.originalname));
-      if (mimetype && extname) {
-        return cb(null, true);
-      }
-      cb("error: el archivo debe ser una imagen valida");
-    },
-  }).single("image");
-
-  mMulter()
- */
   next();
 };
 
