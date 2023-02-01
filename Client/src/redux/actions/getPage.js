45ea74fc343341231b210a62065f0db3bb2beba3
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-02-01 17:46:46 -0500

    prueba de api url

--- Client/src/redux/actions/getPage.js
+++ Client/src/redux/actions/getPage.js
@@ -5,7 +5,7 @@ import { API_URL } from "../../config";
 const getPage = (page, info) => {
   return async function (dispatch) {
     await axios
-      .post(`${API_URL}/pagination`, { page: page, info: info })
+      .post(`proyecto-final-vibes-production.up.railway.app/pagination`, { page: page, info: info })
       .then((res) => dispatch({ type: types.GET_PAGE, payload: res.data }))
       .catch((e) => e.error);
   };
