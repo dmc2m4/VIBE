4eeb761077a1243b8d7f4f1cf07dd253df034442
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-02-02 17:08:27 -0500

    proyecto listo para deploy

--- Client/src/redux/actions/getFavorites.js
+++ Client/src/redux/actions/getFavorites.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import types from "./types"
+import {API_URL} from "../../config";
 
 export const getFavorites = (email) => {
   console.log(email);
     return async function (dispatch) {
       try {
-        const fav = await axios.get(`http://localhost:3001/favorites/${email}`);
+        const fav = await axios.get(`${API_URL}/favorites/${email}`);
         dispatch({
             type: types.GET_FAVORITES,
             payload: fav
