805b782823f2a8dc94bc5a54f6f8395fa6f939d8
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-02-01 19:41:18 -0500

    fetch getpage

--- Client/src/redux/actions/getPage.js
+++ Client/src/redux/actions/getPage.js
@@ -5,8 +5,11 @@ import { API_URL } from "../../config";
 const getPage = (page, info) => {
   console.log(page);
   return async function (dispatch) {
-    await axios
-      .post(`${API_URL}/pagination`, { page: page, info: info })
+    await fetch(`${API_URL}/pagination`, {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({page: page, info: info})
+    })
       .then((res) => dispatch({ type: types.GET_PAGE, payload: res.data }))
       .catch((e) => e.error);
   };
