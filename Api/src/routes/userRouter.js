525212f400acddd9f1213b67aee52f79bdb71ff0
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-02-01 19:58:52 -0500

    log en front

--- Api/src/routes/userRouter.js
+++ Api/src/routes/userRouter.js
@@ -23,7 +23,6 @@ userRouter.post("/login", async (req, res) => {
     const user = await loginUser(req.body);
     res.status(200).send(user)
   }catch(error){
-    // console.log(error);
     res.status(401).send(error.message)
   }
 })
--- Client/src/redux/actions/userLogin.js
+++ Client/src/redux/actions/userLogin.js
@@ -5,12 +5,12 @@ import { API_URL } from "../../config";
 const loginUser = (user) => {
   return async function (dispatch) {
     try {
-      const response = await axios.post(`${API_URL}/user/login`, user);
+      const response = await axios.post(`${API_URL}/user/login`, user).then(res => console.log(res)).catch(err => console.log(err););
       dispatch({
         type: types.LOGIN_USER,
         payload: response.data,
       })
-
+      
     } catch (error) {
       return alert(error.message)
     }
