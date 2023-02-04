ae3355293c29cc79b93d4c9eb22160d453c73c57
vlevillar <vles2345@gmail.com>
2023-02-04 02:13:39 -0300

    filter menwomenshoesaccesories

--- Client/src/components/CategoriesLink/CategoriesLink.jsx
+++ Client/src/components/CategoriesLink/CategoriesLink.jsx
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./CategoriesLink.module.css"
 import womenVibe from "../../assets/womenVibe.png"
 import mochilaVibe from "../../assets/bolsoVibe.png"
 import menVibe from "../../assets/menVibe.png"
 import shoesVibe from "../../assets/shoesVibe.png"
 import { useDispatch, useSelector } from "react-redux";
+import cleanPage from "../../redux/actions/cleanPage";
+import updateFilters from "../../redux/actions/updateFilters";
 
 const CategoriesLink = () => {
-  const dispatch = useDispatch
-  const filters = useSelector((state) => state.Filters);
+  const dispatch = useDispatch();
+  const [newFilters, setNewFilters] = useState({})  
 
   function handleChange(e) {
-    const newFilters = {
-      ...filters,
+    setNewFilters({
       [e.target.name]: e.target.value,
-    };
+    });
+  }
+ 
+  useEffect(() => {
     dispatch(updateFilters(newFilters));
     dispatch(cleanPage());
-  }
+  },[newFilters])
 
   return (
     <>
@@ -27,7 +31,7 @@ const CategoriesLink = () => {
           <div className={style.content}>
             <h2>MEN</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel odit quis, suscipit, odio provident unde porro nam laborum voluptates modi quisquam.</p>
-            <button name="gender" id="male" onChange={handleChange}>Buy now</button>
+            <button name="gender" value="male" onClick={handleChange}>Buy now</button>
           </div>
           <img src={menVibe}/>
         </div>
@@ -37,7 +41,7 @@ const CategoriesLink = () => {
           <div className={style.content}>
             <h2>WOMEN</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel odit quis, suscipit, odio provident unde porro nam laborum voluptates modi quisquam.</p>
-            <button href="#">Buy now</button>
+            <button name="gender" value="female" onClick={handleChange}>Buy now</button>
           </div>
           <img src={womenVibe}/>
         </div>
@@ -47,7 +51,7 @@ const CategoriesLink = () => {
           <div className={style.content}>
             <h2>SHOES</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel odit quis, suscipit, odio provident unde porro nam laborum voluptates modi quisquam.</p>
-            <button href="#">Buy now</button>
+            <button name="category" value="shoes" onClick={handleChange}>Buy now</button>
           </div>
           <img src={shoesVibe}/>
         </div>
@@ -57,7 +61,7 @@ const CategoriesLink = () => {
           <div className={style.content}>
             <h2>ACCESORIES</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel odit quis, suscipit, odio provident unde porro nam laborum voluptates modi quisquam.</p>
-            <button href="#">Buy now</button>
+            <button name="category" value="accesories" onClick={handleChange}>Buy now</button>
           </div>
           <img src={mochilaVibe}/>
         </div>
