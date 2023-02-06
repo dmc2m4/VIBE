da12185963eb55c0865c6a6fc6190a894293e0bb
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-02-06 15:52:21 -0500

    probando mercadopago

--- Client/src/components/MercadoPagoIntegration/MercadoPagoIntegration.jsx
+++ Client/src/components/MercadoPagoIntegration/MercadoPagoIntegration.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
 const FORM_ID = "payment-form";
 
 const MercadoPagoIntegration = ({ items }) => {
