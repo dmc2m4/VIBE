27f1c56c8f94e459036db76da27628208d2cfa74
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-02-06 18:14:04 -0500

    deploy 4

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,6 +1,6 @@
 
-export const API_URL = import.meta.env.VITE_APP_API_URL ;
-
+export const API_URL = "http://localhost:3001";
+ 
 
 // import.meta.env.VITE_APP_API_URL ||
 // "http://localhost:3001"
