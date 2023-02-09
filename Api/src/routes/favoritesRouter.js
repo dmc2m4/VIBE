0b7657a75a50253716a1d1467b4d2c9eea0f14d2
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-02-09 05:51:49 -0500

    arreglando rutas favorites

--- Api/src/routes/favoritesRouter.js
+++ Api/src/routes/favoritesRouter.js
@@ -15,13 +15,7 @@ favoritesRouter.post("/", async (req, res) => {
     res.status(400).send(error.message);
   }
 });
-  try {
-    await postFavorites(req.body);
-    res.status(200).send("product in favorites");
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
+
 
 favoritesRouter.get("/:email", async (req, res) => {
   try {
