2bd476cb820330d8ba8c67c8b17511986c8425b1
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-02-22 07:31:32 -0500

    cambiando config para deploy

--- Api/config.js
+++ Api/config.js
@@ -6,7 +6,7 @@ const DB_NAME = process.env.DB_NAME  || 'vibes'
 const PORT = process.env.PORT   || '3001'
 const DB_DEPLOY = process.env.DB_DEPLOY;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN  ||"APP_USR-12345678-031820-X-12345678";
-const API_URL = process.env.VITE_APP_API_URL || "http://localhost:5173"
+const API_URL = process.env.VITE_APP_API_URL;
 module.exports = {
   DB_USER,
   DB_PASSWORD,
